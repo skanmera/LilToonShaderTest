a9d4ad3d75ebcb7fa02ddaa8452f91bafa8d287e
skanmera <s.kanmera.dev@gmail.com>
2022-11-19 12:07:12 +0900

    initial commit.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.1f, 0f, 0f);
+    }
+}
